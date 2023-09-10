9aa68930942ed10496760fea1fb93072fc6a0d7d
Matchobas <maalexandria@hotmail.com>
2023-09-09 23:13:15 -0300

    feat: slug static create function

--- src/domain/entities/valueObjects/slug.ts
+++ src/domain/entities/valueObjects/slug.ts
@@ -13,6 +13,16 @@ export class Slug {
    * @param text {string}
    */
   static createFromText(text: string) {
-
+    const slugText = text
+      .normalize("NFKD")
+      .toLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w]+/g, '-')
+      .replace(/_/g, '-')
+      .replace(/--/g, '-')
+      .replace(/-$/g, '-')
+      
+    return new Slug(slugText);
   }
 }
