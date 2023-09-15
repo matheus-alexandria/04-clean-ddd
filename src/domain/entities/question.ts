ddcd9d1a90524c559377c0d7a1c8c74fa9ba12bf
Matchobas <maalexandria@hotmail.com>
2023-09-14 23:25:02 -0300

    feat: optional typing

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,13 +1,16 @@
 import { Slug } from "./valueObjects/slug";
 import { Entity } from "../../core/entities/entity";
 import { UniqueEntityID } from "../../core/entities/uniqueEntityId";
+import { Optional } from "../../core/types/optional";
 
 export class Question extends Entity<Question.Params> {
-  static create(props: Question.Params, id?: UniqueEntityID) {
+  static create(props: Optional<Question.Params, 'createdAt'>, id?: UniqueEntityID) {
     const question = new Question({
       ...props,
       createdAt: new Date(),
-    }, id)
+    }, id );
+
+    return question;
   }
 }
 
