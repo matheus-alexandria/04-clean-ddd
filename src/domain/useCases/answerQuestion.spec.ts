77d84fb4846eee118579e26aa27a7a7c0f764708
Matchobas <maalexandria@hotmail.com>
2023-09-23 23:33:40 -0400

    test: new expect

--- src/domain/useCases/answerQuestion.spec.ts
+++ src/domain/useCases/answerQuestion.spec.ts
@@ -1,10 +1,10 @@
 import { AnswerQuestionUseCase } from './answerQuestion';
 import { AnswersRepository } from '../repositories/answersRepository';
 import { Answer } from '../entities/answer';
+import { UniqueEntityID } from '@core/entities/uniqueEntityId';
 
 const fakeAnswersRepository: AnswersRepository = {
 	create: async (answer: Answer): Promise<void> => {
-		console.log(answer);
 		return;
 	}
 };
@@ -13,10 +13,11 @@ it('should be able to create an answer', async () => {
 	const answerQuestionUseCase = new AnswerQuestionUseCase(fakeAnswersRepository);
 
 	const answer = await answerQuestionUseCase.execute({
-		instructorId: '1',
-		questionId: '1',
+		instructorId: 'abdce',
+		questionId: '12345',
 		content: 'Nova resposta'
 	});
 
 	expect(answer.content).toEqual('Nova resposta');
+	expect(answer.authorId).toBeInstanceOf(UniqueEntityID);
 });
