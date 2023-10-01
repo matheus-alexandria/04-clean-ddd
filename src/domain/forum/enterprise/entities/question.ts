c5b489d53369e0b4edcc78634bbc2e36f9c176a3
Matchobas <maalexandria@hotmail.com>
2023-10-01 13:05:48 -0300

    refactor: exporting props interface

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,7 +3,7 @@ import { Entity } from '@core/entities/entity';
 import { UniqueEntityID } from '@core/entities/uniqueEntityId';
 import { Optional } from '@core/types/optional';
 
-interface QuestionProps {
+export interface QuestionProps {
   title: string;
   content: string;
   slug: Slug;
