6fa7a5174394a30b2320a651b7854dad10459a1d
Matchobas <maalexandria@hotmail.com>
2023-10-02 22:55:34 -0300

    refactor: add custom id possibility

--- __tests__/factories/makeQuestion.ts
+++ __tests__/factories/makeQuestion.ts
@@ -1,13 +1,14 @@
 import { UniqueEntityID } from '@core/entities/uniqueEntityId';
 import { Question, QuestionProps } from '@domain/forum/enterprise/entities/question';
+import { faker } from '@faker-js/faker';
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntityID) {
 	const question = Question.create({
 		authorId: new UniqueEntityID(),
-		title: 'Any question',
-		content: 'Any content for this factory question',
+		title: faker.lorem.sentence(),
+		content: faker.lorem.text(),
 		...override
-	});
+	}, id);
 
 	return question;
 }
