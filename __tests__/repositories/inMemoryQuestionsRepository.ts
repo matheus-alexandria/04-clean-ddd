3fa387fb827f67c5aeb587c0dac8f3ab689bba9f
Matchobas <maalexandria@hotmail.com>
2023-10-02 22:55:03 -0300

    feat: create new methods in repository interface and implement inMemory

--- __tests__/repositories/inMemoryQuestionsRepository.ts
+++ __tests__/repositories/inMemoryQuestionsRepository.ts
@@ -1,12 +1,13 @@
 import { UniqueEntityID } from '@core/entities/uniqueEntityId';
 import { QuestionsRepository } from '@domain/forum/application/repositories/questionsRepository';
 import { Question, QuestionProps } from '@domain/forum/enterprise/entities/question';
+import { faker } from '@faker-js/faker';
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
 	public questions: Question[] = [];
 
-	async findBySlug(slug: string): Promise<Question | null> {
-		const question = this.questions.find((data) => data.slug.value === slug);
+	async findById(id: string): Promise<Question | null> {
+		const question = this.questions.find((question) => question.id.toString() === id);
 
 		if (!question) {
 			return null;
@@ -14,21 +15,37 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
 		return question;
 	}
+  
+	async findBySlug(slug: string): Promise<Question | null> {
+		const question = this.questions.find((data) => data.slug.value === slug);
+    
+		if (!question) {
+			return null;
+		}
+    
+		return question;
+	}
 
 	async create(question: Question): Promise<void> {
 		this.questions.push(question);
 	}
 
-	factory(override: Partial<QuestionProps> = {}) {
+	factory(override: Partial<QuestionProps> = {}, id?: UniqueEntityID) {
 		const question = Question.create({
 			authorId: new UniqueEntityID(),
-			title: 'Any question',
-			content: 'Any content for this factory question',
+			title: faker.lorem.sentence(),
+			content: faker.lorem.text(),
 			...override
-		});
+		}, id);
 
 		this.questions.push(question);
-  
+    
 		return question;
+	}
+
+	async delete(question: Question): Promise<void> {
+		const questionIndex = this.questions.findIndex((value) => value.id === question.id);
+
+		this.questions.splice(questionIndex, 1);
 	}
 }
