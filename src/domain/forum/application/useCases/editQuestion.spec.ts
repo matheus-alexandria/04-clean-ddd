bc088ee1bdf65f33a9e8b2867dd1a480a2da976f
Matchobas <maalexandria@hotmail.com>
2023-10-05 22:39:58 -0300

    test: add first edit question tests

---
+++ src/domain/forum/application/useCases/editQuestion.spec.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityID } from '@core/entities/uniqueEntityId';
+import { EditQuestionUseCase } from './editQuestion';
+import { InMemoryQuestionsRepository } from '__tests__/repositories/inMemoryQuestionsRepository';
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: EditQuestionUseCase;
+
+describe('Edit Question', () => {
+	beforeEach(() => {
+		inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+		sut = new EditQuestionUseCase(inMemoryQuestionsRepository);
+	});
+	it('should be able to edit a question', async () => {
+		inMemoryQuestionsRepository.factory(
+			{ authorId: new UniqueEntityID('author-1')}, 
+			new UniqueEntityID('question-1')
+		);
+		await sut.execute({
+			authorId: 'author-1',
+			questionId: 'question-1',
+			title: 'New title',
+			content: 'New updated content'
+		});
+  
+		expect(inMemoryQuestionsRepository.questions[0].slug.value).toEqual('new-title');
+	});
+
+	it('should not be able to edit a question from different author', async () => {
+		inMemoryQuestionsRepository.factory(
+			{ authorId: new UniqueEntityID('author-1')}, 
+			new UniqueEntityID('question-1')
+		);
+  
+		await expect(
+			sut.execute({
+				authorId: 'author-2',
+				questionId: 'question-1',
+				title: 'New title',
+				content: 'New updated content'
+			})
+		).rejects.toBeInstanceOf(Error);
+	});
+});
