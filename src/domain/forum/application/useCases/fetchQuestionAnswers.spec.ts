34cd80d8bd3a69285c6696efd6619362035020f8
Matchobas <maalexandria@hotmail.com>
2023-10-09 10:32:06 -0300

    test: success and pagination test for fetch question answers use case

---
+++ src/domain/forum/application/useCases/fetchQuestionAnswers.spec.ts
@@ -0,0 +1,47 @@
+import { UniqueEntityID } from '@core/entities/uniqueEntityId';
+import { FetchQuestionAnswersUseCase } from './fetchQuestionAnswers';
+import { InMemoryAnswersRepository } from '@test/repositories/inMemoryAnswersRepository';
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: FetchQuestionAnswersUseCase;
+
+describe('Fetch Question Answers', () => {
+	beforeEach(() => {
+		inMemoryAnswersRepository = new InMemoryAnswersRepository();
+		sut = new FetchQuestionAnswersUseCase(inMemoryAnswersRepository);
+	});
+	it('should be able to fetch answers from a question', async () => {
+		inMemoryAnswersRepository.factory(
+			{ questionId: new UniqueEntityID('question-1'), content: 'answer first' }
+		);
+		inMemoryAnswersRepository.factory(
+			{ questionId: new UniqueEntityID('question-1'), content: 'answer second...' }
+		);
+
+		const { answers } = await sut.execute({
+			questionId: 'question-1',
+			page: 1
+		});
+
+		expect(answers).toHaveLength(2);
+		expect(answers).toEqual([
+			expect.objectContaining({ content: 'answer first'}),
+			expect.objectContaining({ content: 'answer second...'})
+		]);
+	});
+
+	it('should be able to fetch answers with pagination', async () => {
+		for (let i = 0; i < 22; i++) {
+			inMemoryAnswersRepository.factory(
+				{ questionId: new UniqueEntityID('question-1') }
+			);
+		}
+
+		const { answers } = await sut.execute({
+			questionId: 'question-1',
+			page: 2
+		});
+  
+		expect(answers).toHaveLength(2);
+	});
+});
