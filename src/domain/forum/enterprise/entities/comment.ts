2a638932f42dbf95de8033e65a6ce0f188f8e6f6
Matchobas <maalexandria@hotmail.com>
2023-10-10 10:15:15 -0300

    feat: create abstract class for comment

---
+++ src/domain/forum/enterprise/entities/comment.ts
@@ -0,0 +1,37 @@
+import { Entity } from '@core/entities/entity';
+import { UniqueEntityID } from '@core/entities/uniqueEntityId';
+
+export interface CommentProps {
+  content: string;
+  authorId: UniqueEntityID;
+  createdAt: Date;
+  updatedAt?: Date;
+}
+export abstract class Comment<
+  Props extends CommentProps
+> extends Entity<Props> {
+	get content() {
+		return this.props.content;
+	}
+
+	set content(content: string) {
+		this.props.content = content;
+	}
+
+	get authorId() {
+		return this.props.authorId;
+	}
+
+	get createdAt() {
+		return this.props.createdAt;
+	}
+
+	get updatedAt() {
+		return this.props.updatedAt;
+	}
+
+	private touch() {
+		this.props.updatedAt = new Date();
+	}
+
+}
