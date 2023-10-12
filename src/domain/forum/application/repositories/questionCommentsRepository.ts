1edcac512a47b9461fa643b62e3e9a23ca0cc1af
Matchobas <maalexandria@hotmail.com>
2023-10-12 16:38:08 -0300

    feat: question comment findById and delete in interface

--- src/domain/forum/application/repositories/questionCommentsRepository.ts
+++ src/domain/forum/application/repositories/questionCommentsRepository.ts
@@ -1,5 +1,7 @@
 import { QuestionComment } from '@domain/forum/enterprise/entities/questionComment';
 
 export interface QuestionCommentsRepository {
+  findById(questionCommentId: string): Promise<QuestionComment | null>;
   create(questionComment: QuestionComment): Promise<void>;
+  delete(questionComment: QuestionComment): Promise<void>;
 }
