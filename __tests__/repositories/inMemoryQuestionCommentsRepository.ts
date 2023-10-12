47a158096cf91acb2e2ce9ec6f1fe400fa76e0ef
Matchobas <maalexandria@hotmail.com>
2023-10-12 16:38:25 -0300

    feat: inMemory methods findById and delete for question comments

--- __tests__/repositories/inMemoryQuestionCommentsRepository.ts
+++ __tests__/repositories/inMemoryQuestionCommentsRepository.ts
@@ -4,7 +4,23 @@ import { QuestionComment } from '@domain/forum/enterprise/entities/questionComme
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
 	public items: QuestionComment[] = [];
 
+	async findById(questionCommentId: string): Promise<QuestionComment | null> {
+		const questionComment = this.items.find((item) => item.id.toString() === questionCommentId);
+
+		if (!questionComment) {
+			return null;
+		}
+
+		return questionComment;
+	}
+  
 	async create(questionComment: QuestionComment): Promise<void> {
 		this.items.push(questionComment);
+	}
+
+	async delete(questionComment: QuestionComment): Promise<void> {
+		const questionCommentIndex = this.items.findIndex((item) => item.id === questionComment.id);
+
+		this.items.splice(questionCommentIndex, 1);
 	}
 }
