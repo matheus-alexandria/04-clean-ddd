2aa5f3f79066a6543166d8e6afd8a527a30e2792
Matchobas <maalexandria@hotmail.com>
2023-10-14 23:09:05 -0400

    feat: answerComment inMemory and rep findById and delete methods

--- __tests__/repositories/inMemoryAnswerCommentsRepository.ts
+++ __tests__/repositories/inMemoryAnswerCommentsRepository.ts
@@ -4,7 +4,23 @@ import { AnswerComment } from '@domain/forum/enterprise/entities/answerComment';
 export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
 	public items: AnswerComment[] = [];
 
+	async findById(answerCommentId: string): Promise<AnswerComment | null> {
+		const answerComment = this.items.find((item) => item.id.toString() === answerCommentId);
+
+		if (!answerComment) {
+			return null;
+		}
+
+		return answerComment;
+	}
+  
 	async create(answerComment: AnswerComment): Promise<void> {
 		this.items.push(answerComment);
+	}
+  
+	async delete(answerComment: AnswerComment): Promise<void> {
+		const answerCommentIndex = this.items.findIndex((item) => item.id === answerComment.id);
+
+		this.items.splice(answerCommentIndex, 1);
 	}
 }
--- src/domain/forum/application/repositories/answerCommentsRepository.ts
+++ src/domain/forum/application/repositories/answerCommentsRepository.ts
@@ -1,5 +1,7 @@
 import { AnswerComment } from '@domain/forum/enterprise/entities/answerComment';
 
 export interface AnswerCommentsRepository {
+  findById(answerCommentId: string): Promise<AnswerComment | null>;
   create(answerComment: AnswerComment): Promise<void>;
+  delete(answerComment: AnswerComment): Promise<void>;
 }
