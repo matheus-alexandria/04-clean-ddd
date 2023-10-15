de7d26b5e008b4bd608dc2d7d5eb76c379b935ae
Matchobas <maalexandria@hotmail.com>
2023-10-15 11:41:03 -0300

    feat: question comment rep and inMemory findManyByQuestionId method

--- __tests__/repositories/inMemoryQuestionCommentsRepository.ts
+++ __tests__/repositories/inMemoryQuestionCommentsRepository.ts
@@ -1,19 +1,28 @@
 import { UniqueEntityID } from '@core/entities/uniqueEntityId';
+import { PaginationParams } from '@core/repositories/PaginationParams';
 import { QuestionCommentsRepository } from '@domain/forum/application/repositories/questionCommentsRepository';
 import { QuestionComment, QuestionCommentProps } from '@domain/forum/enterprise/entities/questionComment';
 import { faker } from '@faker-js/faker';
 
 export class InMemoryQuestionCommentsRepository implements QuestionCommentsRepository {
 	public items: QuestionComment[] = [];
-
+  
 	async findById(questionCommentId: string): Promise<QuestionComment | null> {
 		const questionComment = this.items.find((item) => item.id.toString() === questionCommentId);
-
+    
 		if (!questionComment) {
 			return null;
 		}
-
+    
 		return questionComment;
+	}
+  
+	async findManyByQuestionId(questionId: string, { page }: PaginationParams): Promise<QuestionComment[]> {
+		const questionComments = this.items
+			.filter((item) => item.questionId.toString() === questionId)
+			.slice((page - 1) * 20, page * 20);
+
+		return questionComments;
 	}
   
 	async create(questionComment: QuestionComment): Promise<void> {
--- src/domain/forum/application/repositories/questionCommentsRepository.ts
+++ src/domain/forum/application/repositories/questionCommentsRepository.ts
@@ -1,7 +1,9 @@
+import { PaginationParams } from '@core/repositories/PaginationParams';
 import { QuestionComment } from '@domain/forum/enterprise/entities/questionComment';
 
 export interface QuestionCommentsRepository {
   findById(questionCommentId: string): Promise<QuestionComment | null>;
+  findManyByQuestionId(questionId: string, pagination: PaginationParams): Promise<QuestionComment[]>;
   create(questionComment: QuestionComment): Promise<void>;
   delete(questionComment: QuestionComment): Promise<void>;
 }
