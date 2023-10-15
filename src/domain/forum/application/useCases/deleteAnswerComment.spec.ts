84907daceea6798d700c86b71b17e4b89f15bb74
Matchobas <maalexandria@hotmail.com>
2023-10-14 23:15:06 -0400

    test: success and fail tests for deleteAnswerComment use case

---
+++ src/domain/forum/application/useCases/deleteAnswerComment.spec.ts
@@ -0,0 +1,35 @@
+import { UniqueEntityID } from '@core/entities/uniqueEntityId';
+import { DeleteAnswerCommentUseCase } from './deleteAnswerComment';
+import { InMemoryAnswerCommentsRepository } from '__tests__/repositories/inMemoryAnswerCommentsRepository';
+
+let inMemoryAnswerCommentsRepository: InMemoryAnswerCommentsRepository;
+let sut: DeleteAnswerCommentUseCase;
+
+describe('Delete Answer Comment', () => {
+	beforeEach(() => {
+		inMemoryAnswerCommentsRepository = new InMemoryAnswerCommentsRepository();
+		sut = new DeleteAnswerCommentUseCase(inMemoryAnswerCommentsRepository);
+	});
+	it('should be able to delete a answer comment', async () => {
+		const answerComment = inMemoryAnswerCommentsRepository.factory();
+		await sut.execute({
+			authorId: answerComment.authorId.toString(),
+			answerCommentId: answerComment.id.toString(),
+		});
+  
+		expect(inMemoryAnswerCommentsRepository.items).toHaveLength(0);
+	});
+
+	it('should not be able to delete answer comment from different author', async () => {
+		const answerComment = inMemoryAnswerCommentsRepository.factory(
+			{ authorId: new UniqueEntityID('author-1')}
+		);
+  
+		await expect(
+			sut.execute({
+				authorId: 'author-2',
+				answerCommentId: answerComment.id.toString(),
+			})
+		).rejects.toThrowError();
+	});
+});
