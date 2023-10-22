b754b1abca4dd83015b3a16eeb70916270e3eec2
Matchobas <maalexandria@hotmail.com>
2023-10-22 20:50:13 -0300

    feat: in memory question attachment repository created

---
+++ __tests__/repositories/inMemoryQuestionAttachmentsRepository.ts
@@ -0,0 +1,21 @@
+import { UniqueEntityID } from '@core/entities/uniqueEntityId';
+import { QuestionAttachmentsRepository } from '@domain/forum/application/repositories/questionAttachmentsRepository';
+import { QuestionAttachment, QuestionAttachmentProps } from '@domain/forum/enterprise/entities/questionAttachment';
+
+export class InMemoryQuestionAttachmentsRepository implements QuestionAttachmentsRepository {
+	public attachments: QuestionAttachment[] = [];
+	async findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]> {
+		return this.attachments.filter((item) => item.questionId.toString() === questionId);
+	}
+	factory(override: Partial<QuestionAttachmentProps> = {}, id?: UniqueEntityID) {
+		const question = QuestionAttachment.create({
+			attachmentId: new UniqueEntityID(),
+			questionId: new UniqueEntityID(),
+			...override
+		}, id);
+
+		this.attachments.push(question);
+    
+		return question;
+	}
+}
