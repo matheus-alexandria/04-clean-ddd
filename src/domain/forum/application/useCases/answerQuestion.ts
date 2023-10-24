effd996643390ba1676bb0d5c4420a6f3893b034
Matchobas <maalexandria@hotmail.com>
2023-10-24 10:18:44 -0300

    feat: add answer attachment list method to answer question

--- src/domain/forum/application/useCases/answerQuestion.ts
+++ src/domain/forum/application/useCases/answerQuestion.ts
@@ -2,16 +2,36 @@ import { UniqueEntityID } from '@core/entities/uniqueEntityId';
 import { Answer } from '@domain/forum/enterprise/entities/answer';
 import { AnswersRepository } from '../repositories/answersRepository';
 import { Either, right } from '@core/either';
+import { AnswerAttachment } from '@domain/forum/enterprise/entities/answerAttachment';
+import { AnswerAttachmentList } from '@domain/forum/enterprise/entities/answerAttachmentList';
 
 export class AnswerQuestionUseCase {
 	constructor(
     private answersRepository: AnswersRepository
 	) {}
 
-	async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
+	async execute({ 
+		instructorId, 
+		questionId, 
+		content, 
+		attachmentIds 
+	}: AnswerQuestionUseCaseRequest): Promise<AnswerQuestionUseCaseResponse> {
 		const authorId = new UniqueEntityID(instructorId);
 		const questionUniqueId = new UniqueEntityID(questionId);
-		const answer = Answer.create({authorId, questionId: questionUniqueId, content});
+		const answer = Answer.create({
+			authorId, 
+			questionId: questionUniqueId, 
+			content
+		});
+
+		const attachments = attachmentIds.map((attachmentId) => {
+			return AnswerAttachment.create({
+				answerId: answer.id,
+				attachmentId: new UniqueEntityID(attachmentId)
+			});
+		});
+
+		answer.attachments = new AnswerAttachmentList(attachments);
 
 		await this.answersRepository.create(answer);
 
@@ -25,6 +45,7 @@ interface AnswerQuestionUseCaseRequest {
   instructorId: string;
   questionId: string;
   content: string;
+  attachmentIds: string[];
 }
 
 type AnswerQuestionUseCaseResponse = Either<null, { answer: Answer }>;
