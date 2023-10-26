4bee2afce7e0454be069243d5d7ae4185927da50
Matchobas <maalexandria@hotmail.com>
2023-10-26 09:57:05 -0300

    feat: save and findById methods for notification rep and inMemory

--- __tests__/repositories/inMemoryNotificationsRepository.ts
+++ __tests__/repositories/inMemoryNotificationsRepository.ts
@@ -5,9 +5,25 @@ import { faker } from '@faker-js/faker';
 
 export class InMemoryNotificationsRepository implements NotificationsRepository {
 	public notifications: Notification[] = [];
+	
+	async findById(notificationId: string): Promise<Notification | null> {
+		const notification = this.notifications.find((item) => item.id.toString() === notificationId);
+
+		if (!notification) {
+			return null;
+		}
+
+		return notification;
+	}
 
 	async create(notification: Notification): Promise<void> {
 		this.notifications.push(notification);
+	}
+
+	async save(notification: Notification): Promise<void> {
+		const notificationIndex = this.notifications.findIndex((item) => notification.id === item.id);
+
+		this.notifications[notificationIndex] = notification;
 	}
 
 	factory(override: Partial<NotificationProps> = {}, id?: UniqueEntityID) {
