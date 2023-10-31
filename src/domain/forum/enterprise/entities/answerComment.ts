af84b1d3639ea30b6069afe00e4bd48e3cfb6e2b
Matchobas <maalexandria@hotmail.com>
2023-10-31 10:33:58 -0300

    feat: add domain event in answer coment creation

--- src/domain/forum/enterprise/entities/answerComment.ts
+++ src/domain/forum/enterprise/entities/answerComment.ts
@@ -1,6 +1,7 @@
 import { UniqueEntityID } from '@core/entities/uniqueEntityId';
 import { Optional } from '@core/types/optional';
 import { Comment, CommentProps } from './comment';
+import { CommentOnAnswerEvent } from './commentOnAnswerEvent';
 
 export interface AnswerCommentProps extends CommentProps {
   answerId: UniqueEntityID;
@@ -9,12 +10,21 @@ export class AnswerComment extends Comment<AnswerCommentProps> {
 	get answerId() {
 		return this.props.answerId;
 	}
+  
+	get excerpt() {
+		return this.content.substring(0,120).trimEnd().concat('...');
+	}
 
 	static create(props: Optional<AnswerCommentProps, 'createdAt'>, id?: UniqueEntityID) {
 		const answerComment = new AnswerComment({
 			...props,
 			createdAt: props.createdAt ?? new Date()
 		}, id);
+
+		if (!id) {
+			answerComment.addDomainEvent(new CommentOnAnswerEvent(answerComment));
+		}
+
 		return answerComment;
 	}
 }
